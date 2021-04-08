2369c312c06f79721402b9fee91398c022915b6f
Malcomputer <malcolmmkubwa@gmail.com>
2021-04-08 09:06:34 -0500

    Login function component

--- src/views/Login.js
+++ src/views/Login.js
@@ -1,109 +1,104 @@
 import "../assets/login.css";
-import React from "react";
+import logo from '../images/logo.png';
+import {Link, useHistory} from 'react-router-dom';
+import {Button, Form, Grid, Header, Message, Segment} from "semantic-ui-react";
+import {useEffect, useState} from "react";
+import {loginRequest} from "../Api";
+import {ACTIONS, useStore} from "../store";
 
-import {
-  Grid,
-  Form,
-  Segment,
-  Button,
-  Header,
-  Message,
-  Icon,
-} from "semantic-ui-react";
-import { Link } from "react-router-dom";
+function Login() {
+	const history = useHistory();
+	const {dispatch} = useStore();
+	const [state, setState] = useState({
+		username: "",
+		password: "",
+		errors: [],
+		loading: false
+	});
+	const {username, password, errors, loading} = state;
 
-class Login extends React.Component {
-  state = {
-    email: "",
-    password: "",
-    errors: [],
-    loading: false,
-  };
+	useEffect(() => {
+		if (localStorage.getItem(ACTIONS.TOKEN)) history.push('/');
+	});
 
-  displayErrors = (errors) =>
-    errors.map((error, i) => <p key={i}>{error.message}</p>);
+	const displayErrors = (errors) => errors.map((error, i) => <p key={i}>{error.message}</p>);
 
-  handleChange = (event) => {
-    this.setState({ [event.target.name]: event.target.value });
-  };
+	const handleChange = (event) => {
+		setState(state => ({...state, ...{[event.target.name]: event.target.value}}));
+	};
 
-  handleSubmit = (event) => {
-    event.preventDefault();
-    if (this.isFormValid(this.state)) {
-      this.setState({ errors: [], loading: true });
-    }
-  };
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		if (isFormValid(state)) {
+			setState({errors: [], loading: true});
+			loginRequest(state.username, state.password).then(data => {
+				if (data.error) return setState({errors: [{message: data.error.message}]});
+				dispatch({type: ACTIONS.SETUSER, data});
+			});
+		}
+	};
 
-  isFormValid = ({ email, password }) => email && password;
+	const isFormValid = ({username, password}) => username && password;
 
-  handleInputError = (errors, inputName) => {
-    return errors.some((error) =>
-      error.message.toLowerCase().includes(inputName)
-    )
-      ? "error"
-      : "";
-  };
+	const handleInputError = (errors, inputName) => {
+		return errors?.some((error) => error.message.toLowerCase().includes(inputName)) ? "error" : "";
+	};
 
-  render() {
-    const { email, password, errors, loading } = this.state;
+	return (
+		<Grid textAlign="center" verticalAlign="middle" className="login-form">
+			<Grid.Column style={{maxWidth: 450}}>
+				<Header as="h1" icon textAlign="center">
+					<img src={logo} alt="NanobeezZone" />
+					Login to NanoBeezZone
+				</Header>
+				<Form onSubmit={handleSubmit} size="large">
+					<Segment stacked>
+						<Form.Input
+							fluid
+							name="username"
+							icon="user"
+							iconPosition="left"
+							placeholder="Username"
+							onChange={handleChange}
+							value={username}
+							className={handleInputError(errors, "username")}
+							type="text"
+						/>
 
-    return (
-      <Grid textAlign="center" verticalAlign="middle" className="app">
-        <Grid.Column style={{ maxWidth: 450 }}>
-          <Header as="h1" icon color="violet" textAlign="center">
-            <Icon name="code branch" color="violet" />
-            Login to NanobeezZone
-          </Header>
-          <Form onSubmit={this.handleSubmit} size="large">
-            <Segment stacked>
-              <Form.Input
-                fluid
-                name="email"
-                icon="mail"
-                iconPosition="left"
-                placeholder="Email Address"
-                onChange={this.handleChange}
-                value={email}
-                className={this.handleInputError(errors, "email")}
-                type="email"
-              />
+						<Form.Input
+							fluid
+							name="password"
+							icon="lock"
+							iconPosition="left"
+							placeholder="Password"
+							onChange={handleChange}
+							value={password}
+							className={handleInputError(errors, "password")}
+							type="password"
+						/>
 
-              <Form.Input
-                fluid
-                name="password"
-                icon="lock"
-                iconPosition="left"
-                placeholder="Password"
-                onChange={this.handleChange}
-                value={password}
-                className={this.handleInputError(errors, "password")}
-                type="password"
-              />
-
-              <Button
-                disabled={loading}
-                className={loading ? "loading" : ""}
-                color="violet"
-                fluid
-                size="large"
-              >
-                Submit
-              </Button>
-            </Segment>
-          </Form>
-          {errors.length > 0 && (
-            <Message error>
-              <h3>Error</h3>
-              {this.displayErrors(errors)}
-            </Message>
-          )}
-          <Message>
-            Don't have an account? <Link to="/Signup">Signup</Link>
-          </Message>
-        </Grid.Column>
-      </Grid>
-    );
-  }
+						<Button
+							disabled={loading}
+							className={loading ? "loading" : ""}
+							fluid
+							size="large"
+						>
+							Submit
+						</Button>
+					</Segment>
+				</Form>
+				{errors?.length > 0 && (
+					<Message error>
+						<h3>Error</h3>
+						{displayErrors(errors)}
+					</Message>
+				)}
+				<Message>
+					Don't have an account? <Link to="/Signup">Signup</Link>
+				</Message>
+			</Grid.Column>
+		</Grid>
+	);
 }
 
 export default Login;
