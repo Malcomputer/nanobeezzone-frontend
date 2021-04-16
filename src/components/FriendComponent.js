640cfa6862671199ad257e0cc5c642e6f03fd1e9
Malcomputer <malcolmmkubwa@gmail.com>
2021-04-16 01:01:49 -0500

    get correct user for userInfoCard

--- src/components/FriendComponent.js
+++ src/components/FriendComponent.js
@@ -1,11 +1,12 @@
 import '../assets/friendcomponent.css';
 import profilePlaceHolder from '../images/profile-placeholder.png';
+import {Link} from "react-router-dom";
 function FriendComponent(props) {
 	return (
-		<div id='user'>
+		<Link to={`/chat/${props.username}`} id='user'>
 			<img id='user-image' src={props.img ? props.img : profilePlaceHolder} alt={props.username} />
 			<span id='user-name'>{props.name}</span>
-		</div>
+		</Link>
 	)
 }
 
--- src/components/UserInfoCard.js
+++ src/components/UserInfoCard.js
@@ -1,46 +1,21 @@
 import profilePlaceholder from '../images/profile-placeholder.png';
-import React, { useEffect } from "react";
+import React, {useEffect, useState} from "react";
 import { Card, Spin, Collapse, Divider } from "antd";
 import { useStore } from "../store/index";
-import { getUserInfo, getUserPicture } from "../Api";
+import {getUser, getUserInfo, getUserPicture} from "../Api";
 import { ACTIONS } from "../store/index";
 import UploadPictureForm from "./UploadPictureForm";
 import DeleteUserButton from "./DeleteUserButton";
-import placeholder from "../images/Placeholder.png";
 import UpdateUserForm from "./UpdateUserForm";
 
 // import CollapsePanel from "antd/lib/collapse/CollapsePanel";
 
 function UserInfoCard(props) {
-  const {username, name, about, img} = useStore((state) => state.currentUser);
-  const profilePagePicture = useStore((state) => state.profilePagePicture);
-  const dispatch = useStore((state) => state.dispatch);
-
+  const currentUser = useStore((state) => state.currentUser);
+  const [{username, name, about, img, error}, setState] = useState(currentUser);
   useEffect(() => {
-    getUserInfo(props.match.params.username).then((userData) => {
-      dispatch({ type: ACTIONS.GET_USER_INFO, payload: userData });
-    });
-    // getUserPicture(props.match.params.username, Date.now()).then((data) => {
-    //   console.log(data);
-    //   if (data.status === 200) {
-    //     dispatch({
-    //       type: ACTIONS.GET_PROFILE_PICTURE,
-    //       payload: data.url,
-    //     });
-    //   }
-    //   if (data.status === 404) {
-    //     dispatch({
-    //       type: ACTIONS.ACTIONS.GET_PROFILE_PICTURE,
-    //       payload: placeholder,
-    //     });
-    //   }
-    // });
-    return () => {
-      dispatch({ type: ACTIONS.GET_PROFILE_PICTURE, payload: null });
-      dispatch({ type: ACTIONS.GET_USER_INFO, payload: null });
-    };
-  }, [props.match.params.username]);
-
+    if (props.match.params.profile !== currentUser.username) getUser(props.match.params.profile).then(setState);
+  }, [props.match.params.profile]);
   return (
     <div className="userInfoCard">
       <Card
@@ -51,8 +26,8 @@ function UserInfoCard(props) {
           title={username}
           description={name}
         />
-        <p>{about || 'This User Hasn\'t Given Us Anything To Put Here!'}</p>
-        {props.match.params.profile === username && (
+        {error?.status === 404 ?<p style={{textAlign: 'center'}}>This user does not exist</p> : <p style={{textAlign: 'center'}}>{about || 'This User Hasn\'t Given Us Anything To Put Here!'}</p>}
+        {props.match.params.profile === currentUser.username && (
           <>
             <Divider orientation="left">User Settings</Divider>
             <Collapse>
