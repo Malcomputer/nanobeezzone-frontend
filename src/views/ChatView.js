077f78e62033d3fd88e1a28cf1054a430ed6dae2
Malcomputer <malcolmmkubwa@gmail.com>
2021-04-18 21:34:35 -0500

    message input textarea

--- src/views/ChatView.js
+++ src/views/ChatView.js
@@ -1,7 +1,24 @@
+import {useEffect} from "react";
+import "../assets/chatview.css";
+import {IoSend} from "react-icons/all";
 
-function ChatView() {
+function ChatView(props) {
+	useEffect(() => {
+		console.log(props.match.params.profile);
+	});
+	const expandTextArea = ({target}) => {
+		if (!target.nodeName === 'TEXTAREA') return
+		target.style.height = '1px';
+		target.style.height = `${target.scrollHeight+2}px`;
+		// console.log(target.scrollHeight);
+	}
 	return (
-		<></>
+		<div id="chat-view">
+			<div id="text-box">
+				<textarea id="chat-textbox" onKeyUp={expandTextArea} />
+				<div className="icon send"><IoSend /></div>
+			</div>
+		</div>
 	)
 }
 
